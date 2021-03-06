5be39282eeb63a02c102830f33df40a4bcb7ff4b
edris gemtessa <edogemtessa@gmail.com>
2021-03-05 20:09:19 -0600

    3. List the manager of each department with the following information:

--- Employee.sql
+++ Employee.sql
@@ -13,7 +13,7 @@ select * from salaries;
 
 
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-
+========================================================================================================================================
 select  employees.emp_no,
 	    employees.last_name,
 	    employees.first_name,
@@ -22,7 +22,7 @@ select  employees.emp_no,
 from employees
  join salaries on salaries.salary =employees.emp_no ;
 
-
+=========================================================================================================================================
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 select   first_name, 
 		 last_name,
@@ -30,7 +30,27 @@ select   first_name,
 from     employees
 WHERE hire_date BETWEEN '1/1/1986' AND '1/1/1987';
 
+============================================================================================================================================
+--3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
 
+select * from departments;
+select * from employees;
+select * from dept_manger;
+
+
+select 		departments.dept_no,
+			departments.dept_name,
+			dept_manger.emp_no,
+			employees.first_name,
+			employees.last_name
+from       departments
+left join dept_manger
+ on       departments.dept_no =dept_manger.dept_no
+left join employees 
+on         dept_manger.emp_no = employees.emp_no;
+
+=================================================================================================================================================
 
 
 
