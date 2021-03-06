d75b438598b6d23691065b624fc7bc8251485fab
edris gemtessa <edogemtessa@gmail.com>
2021-03-05 18:49:18 -0600

    sql query file created

---
+++ SqlQuery.sql
@@ -0,0 +1,10 @@
+-- checking all table data after imported
+select * from departments;
+select * from dept_emp;
+select * from titles;
+select * from employees;
+select * from dept_manger;
+select * from salaries;
+
+
+
