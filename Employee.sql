d9f72ee06bb0acfbfbfd2ca613781e49cd093b25
edris gemtessa <edogemtessa@gmail.com>
2021-03-05 21:12:52 -0600

    8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.

--- Employee.sql
+++ Employee.sql
@@ -106,6 +106,17 @@ on           departments.dept_no =dept_manger.dept_no
 where     dept_name = 'Development' or dept_name= 'Sales';
 
 
+============================================================================================================================================================
+
+--8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+select last_name,count(last_name) 
+from employees
+group by last_name
+order by last_name desc;
+
+
+
 
 
 
