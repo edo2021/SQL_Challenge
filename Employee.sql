ac94f8455a3dd3788838b20cddb994540d9238c7
edris gemtessa <edogemtessa@gmail.com>
2021-03-05 20:42:27 -0600

    5.List first name, last name, and sex for employees whose first name is Hercules and last names begin with B.

--- Employee.sql
+++ Employee.sql
@@ -62,6 +62,16 @@ on         dept_manger.emp_no = employees.emp_no
 left join departments
  on       departments.dept_no =dept_manger.dept_no;
 
+==================================================================================================================================================================
+--5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+select 		first_name,
+			last_name,
+			sex
+from 		employees
+where first_name =  'Hercules'
+and last_name like 'B%'
+
 
 
 
